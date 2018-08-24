9e43262c31f4fe135cdb4b19d426abeee62c354b
alex-avila <alex.avilx@icloud.com>
2018-08-24 14:37:38 -0600

    Add nav html

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
+import { Link } from 'react-router-dom'
+
 class Nav extends Component {
     render() {
         return (
             <div className="nav">
                 <div className="nav__wrapper">
-                    {/* Logo */}
-                    {/* Other nav items */}
+                    <Link to="/">myagent.io</Link>
+                    <a href="tel:787878787">(878) 981 099</a>
                 </div>
             </div>
         )
