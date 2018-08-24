1e4d96c9ce616f6baaa4f9e05ce5cf8a324d03a7
alex-avila <alex.avilx@icloud.com>
2018-08-24 11:58:36 -0600

    Add redux provider and store and react router to render method

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+import store from './redux'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
