cda8db90998aea6fd9282dd982ac558cd9697262
alex-avila <alex.avilx@icloud.com>
2018-08-24 14:27:42 -0600

    Refactored using a mode variable to use less components

--- client/src/components/FormOptions/PropertyType/index.js
+++ client/src/components/FormOptions/PropertyType/index.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 
 class PropertyType extends Component {
     render() {
+        const { mode } = this.props
+        const link = mode === 'buyer' ? 'budget' : 'price'
         return (
             <div>
                 <div>PropertyType</div>
-                <Link to="budget">Budget</Link>
+                <Link to={link}>Money</Link>
             </div>
         )
     }
