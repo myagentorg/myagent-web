2b2d964be35dfecb99c70ceea4d8b54f34b1619a
alex-avila <alex.avilx@icloud.com>
2018-08-25 15:27:08 -0600

    Add a slider

--- client/src/components/FormOptions/Money/index.js
+++ client/src/components/FormOptions/Money/index.js
@@ -4,30 +4,60 @@ import { Link } from 'react-router-dom'
 
 import Button from '../../Button'
 
+import './index.css'
+
 class Money extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            moneyRange: 250000
+        }
         this.content = {
             buyer: {
                 link: 'also-selling',
                 linkText: 'Also Selling',
-                title: 'Budget'
+                title: 'What’s your approximate budget?'
             },
             seller: {
                 link: 'address',
                 linkText: 'Address',
-                title: 'Price'
+                title: 'Roughly how much is your property worth?'
             }
         }
     }
+
+    handleChange = e => {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
+    }
+
     render() {
         const {
+            props: { mode },
+            state: { moneyRange },
             content,
-            props: { mode }
+            handleChange
         } = this
+        const moneyDisplay =
+            moneyRange <= 0
+                ? `I'm not sure...`
+                : moneyRange < 1000000
+                    ? moneyRange
+                    : '1000000+'
         return (
             <div>
                 <h2>{content[mode].title}</h2>
+                <h3 style={{ fontSize: '2em' }}>{moneyDisplay}</h3>
+                <input
+                    className="money__slider"
+                    name="moneyRange"
+                    type="range"
+                    min="0"
+                    max="1000000"
+                    step="10000"
+                    value={moneyRange}
+                    onChange={handleChange}
+                />
                 <Link to={content[mode].link}>
                     <Button>Next</Button>
                 </Link>
