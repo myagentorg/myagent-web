010b9fe522cc3b629f62beb83215aa6dce8340e6
alex-avila <alex.avilx@icloud.com>
2018-08-25 10:55:45 -0600

    Put radio group in PropertyType into its own component

---
+++ client/src/components/FormOptions/PropertyType/components/RadioGroup/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom'
+import Button from '../../../../Button'
+
+class RadioGroup extends Component {
+    render() {
+        const { mode, type, handleChange } = this.props
+        return (
+            <form>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="single family"
+                            checked={type === 'single family'}
+                            onChange={handleChange}
+                        />
+                        Single-Family
+                    </label>
+                </div>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="condominium"
+                            checked={type === 'condominium'}
+                            onChange={handleChange}
+                        />
+                        Condominium
+                    </label>
+                </div>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="townhouse"
+                            checked={type === 'townhouse'}
+                            onChange={handleChange}
+                        />
+                        Townhouse
+                    </label>
+                </div>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="vacant lot"
+                            checked={type === 'vacant lot'}
+                            onChange={handleChange}
+                        />
+                        Vacant Lot
+                    </label>
+                </div>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="commercial"
+                            checked={type === 'commercial'}
+                            onChange={handleChange}
+                        />
+                        Commercial
+                    </label>
+                </div>
+                <div className="property-type__radio">
+                    <label>
+                        <input
+                            name="type"
+                            type="radio"
+                            value="other"
+                            checked={type === 'other'}
+                            onChange={handleChange}
+                        />
+                        Other
+                    </label>
+                </div>
+                <Link to={mode === 'buyer' ? 'budget' : 'price'}>
+                    <Button>Next</Button>
+                </Link>
+            </form>
+        )
+    }
+}
+
+export default RadioGroup
--- client/src/components/FormOptions/PropertyType/index.js
+++ client/src/components/FormOptions/PropertyType/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
-import { Link } from 'react-router-dom'
-import Button from '../../Button'
+import RadioGroup from './components/RadioGroup'
 
 class PropertyType extends Component {
     constructor(props) {
@@ -26,83 +25,11 @@ class PropertyType extends Component {
                     What type of property are you
                     {mode === 'buyer' ? ' looking for' : ' selling'}?
                 </h2>
-                <form>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="single family"
-                                checked={type === 'single family'}
-                                onChange={handleChange}
-                            />
-                            Single-Family
-                        </label>
-                    </div>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="condominium"
-                                checked={type === 'condominium'}
-                                onChange={handleChange}
-                            />
-                            Condominium
-                        </label>
-                    </div>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="townhouse"
-                                checked={type === 'townhouse'}
-                                onChange={handleChange}
-                            />
-                            Townhouse
-                        </label>
-                    </div>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="vacant lot"
-                                checked={type === 'vacant lot'}
-                                onChange={handleChange}
-                            />
-                            Vacant Lot
-                        </label>
-                    </div>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="commercial"
-                                checked={type === 'commercial'}
-                                onChange={handleChange}
-                            />
-                            Commercial
-                        </label>
-                    </div>
-                    <div className="property-type__radio">
-                        <label>
-                            <input
-                                name="type"
-                                type="radio"
-                                value="other"
-                                checked={type === 'other'}
-                                onChange={handleChange}
-                            />
-                            Other
-                        </label>
-                    </div>
-                    <Link to={mode === 'buyer' ? 'budget' : 'price'}>
-                        <Button>Next</Button>
-                    </Link>
-                </form>
+                <RadioGroup
+                    mode={mode}
+                    type={type}
+                    handleChange={handleChange}
+                />
             </div>
         )
     }
