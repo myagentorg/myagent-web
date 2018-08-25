5f4ec9df296e5b02764cbe6f5dda56e7fc2bf62b
alex-avila <alex.avilx@icloud.com>
2018-08-25 15:38:54 -0600

    Add Input to address component

--- client/src/components/FormOptions/Address/index.js
+++ client/src/components/FormOptions/Address/index.js
@@ -3,15 +3,21 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import Button from '../../Button'
+import Input from '../../Input'
+
+import './index.css'
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <h2>Address</h2>
-                <Link to="also-buying">
-                    <Button>Next</Button>
-                </Link>
+                <h2>What is the address of the property you’re selling?</h2>
+                <div className="address__input-n-button">
+                    <Input placeholder="Search for your address..." required />
+                    <Link to="also-buying">
+                        <Button>Next</Button>
+                    </Link>
+                </div>
             </div>
         )
     }
