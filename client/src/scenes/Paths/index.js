11d43027bcb9b74afa80b7132cc0bb4b69d9f4d9
alex-avila <alex.avilx@icloud.com>
2018-08-26 16:29:52 -0600

    Remove React.Fragment

--- client/src/scenes/Paths/index.js
+++ client/src/scenes/Paths/index.js
@@ -13,67 +13,63 @@ class Paths extends Component {
     render() {
         const { mode } = this.props
         return (
-            <React.Fragment>
-                <Switch>
-                    <Route
-                        path={`/${mode}/property-type`}
-                        render={props => (
-                            <FormContainer {...props}>
-                                <PropertyType mode={mode} />
-                            </FormContainer>
-                        )}
-                    />
-                    <Route
-                        path={
-                            mode === 'buyer'
-                                ? `/${mode}/budget`
-                                : `/${mode}/price`
-                        }
-                        render={props => (
-                            <FormContainer {...props}>
-                                <Money mode={mode} />
-                            </FormContainer>
-                        )}
-                    />
-                    {/* Address is exclusive to seller mode */}
-                    <Route
-                        path={`/${mode}/address`}
-                        render={props => (
-                            <FormContainer {...props}>
-                                <Address />
-                            </FormContainer>
-                        )}
-                    />
-                    <Route
-                        path={
-                            mode === 'buyer'
-                                ? `/${mode}/also-selling`
-                                : `/${mode}/also-buying`
-                        }
-                        render={props => (
-                            <FormContainer {...props}>
-                                <Loan mode={mode} />
-                            </FormContainer>
-                        )}
-                    />
-                    <Route
-                        path={`/${mode}/contact`}
-                        render={props => (
-                            <FormContainer {...props}>
-                                <Contact />
-                            </FormContainer>
-                        )}
-                    />
-                    <Route
-                        path={`/${mode}/complete`}
-                        render={props => (
-                            <FormContainer {...props}>
-                                <Complete />
-                            </FormContainer>
-                        )}
-                    />
-                </Switch>
-            </React.Fragment>
+            <Switch>
+                <Route
+                    path={`/${mode}/property-type`}
+                    render={props => (
+                        <FormContainer {...props}>
+                            <PropertyType mode={mode} />
+                        </FormContainer>
+                    )}
+                />
+                <Route
+                    path={
+                        mode === 'buyer' ? `/${mode}/budget` : `/${mode}/price`
+                    }
+                    render={props => (
+                        <FormContainer {...props}>
+                            <Money mode={mode} />
+                        </FormContainer>
+                    )}
+                />
+                {/* Address is exclusive to seller mode */}
+                <Route
+                    path={`/${mode}/address`}
+                    render={props => (
+                        <FormContainer {...props}>
+                            <Address />
+                        </FormContainer>
+                    )}
+                />
+                <Route
+                    path={
+                        mode === 'buyer'
+                            ? `/${mode}/also-selling`
+                            : `/${mode}/also-buying`
+                    }
+                    render={props => (
+                        <FormContainer {...props}>
+                            <Loan mode={mode} />
+                        </FormContainer>
+                    )}
+                />
+                <Route
+                    path={`/${mode}/contact`}
+                    render={props => (
+                        <FormContainer {...props}>
+                            <Contact />
+                        </FormContainer>
+                    )}
+                />
+                <Route
+                    path={`/${mode}/complete`}
+                    render={props => (
+                        <FormContainer {...props}>
+                            <Complete />
+                        </FormContainer>
+                    )}
+                />
+            </Switch>
         )
     }
 }
