17b7321860ff7ec8380156e042736a13cb02a919
alex-avila <alex.avilx@icloud.com>
2018-08-27 17:08:07 -0600

    Connect to redux form

--- client/src/components/FormOptions/Loan/index.js
+++ client/src/components/FormOptions/Loan/index.js
@@ -8,19 +8,31 @@ import './index.css'
 
 class Loan extends Component {
     render() {
+        const { handleAddNewField, field } = this.props
         return (
             <div>
                 <h2>Are you approved for a loan?</h2>
                 <div className="loan__buttons">
-                    <Link to="contact">
+                    <Link
+                        to="contact"
+                        onClick={() => handleAddNewField(field, 'approved')}
+                    >
                         <Button>I'm approved</Button>
                     </Link>
-                    <Link to="contact">
+                    <Link
+                        to="contact"
+                        onClick={() => handleAddNewField(field, 'need lender')}
+                    >
                         <Button>I need a lender</Button>
                     </Link>
                 </div>
                 <div>
-                    <Link to="contact">No thanks</Link>
+                    <Link
+                        to="contact"
+                        onClick={() => handleAddNewField(field, 'no thanks')}
+                    >
+                        No thanks
+                    </Link>
                 </div>
             </div>
         )
