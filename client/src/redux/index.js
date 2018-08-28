6519685c826fa9d58e5074e2601d6b5e63f293a2
alex-avila <alex.avilx@icloud.com>
2018-08-28 17:37:59 -0600

    Remove proxy and unnecessary reducer

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
-import defaultReducer from './reducers/defaultReducer'
 import form from './reducers/formReducer'
 
-const rootReducer = combineReducers({ defaultReducer, form })
+const rootReducer = combineReducers({ form })
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
