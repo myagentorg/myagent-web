f3fdaa38d5dda324c2ad656f995863a82246f8c2
alex-avila <alex.avilx@icloud.com>
2018-08-27 21:35:00 -0600

    Add rest of fields for 'record' entry

--- models/record.js
+++ models/record.js
@@ -2,10 +2,28 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const recordSchema = new Schema({
+    path: {
+        type: String,
+        enum: ['buyer', 'seller', 'buyer-and-seller']
+    },
     contact: {
         name: {
             type: String,
+            trim: true,
             required: true
+        },
+        email: {
+            type: String,
+            lowercase: true,
+            trim: true,
+            required: true
+        },
+        phone: {
+            type: Number
+        },
+        additionalInfo: {
+            type: String,
+            trim: true
         }
     },
     location: {
@@ -17,6 +35,63 @@ const recordSchema = new Schema({
             type: Number,
             required: true
         }
+    },
+    budget: {
+        type: Number
+    },
+    price: {
+        type: Number
+    },
+    address: {
+        lat: {
+            type: Number,
+            required: true
+        },
+        lng: {
+            type: Number,
+            required: true
+        }
+    },
+    propertyType: {
+        type: String,
+        lowercase: true,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    propertyTypeBuyer: {
+        type: String,
+        lowercase: true,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    propertyTypeSeller: {
+        type: String,
+        lowercase: true,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    loan: {
+        type: String,
+        require: true,
+        enum: ['no thanks', 'approved', 'need lender']
     }
 })
 
