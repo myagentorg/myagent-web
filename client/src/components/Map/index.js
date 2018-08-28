beaf2d0418a98cff07931b37e71bacc98456ac80
alex-avila <alex.avilx@icloud.com>
2018-08-27 19:49:24 -0600

    Create Map component with react-google-maps

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+
+const Map = withGoogleMap(props => (
+    <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{
+            lat: props.lat || 39.7392,
+            lng: props.lng || -104.9903
+        }}
+    >
+        {props.isMarkerShown && (
+            <Marker
+                position={{
+                    lat: props.lat || 39.7392,
+                    lng: props.lng || -104.9903
+                }}
+            />
+        )}
+    </GoogleMap>
+))
+
+export default Map
