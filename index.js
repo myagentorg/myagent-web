8ef6e5c94a5dd6c5496df5a5d99a4ebe043770dc
alex-avila <alex.avilx@icloud.com>
2018-08-28 21:25:51 -0600

    Add mongodb uri

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 mongoose.connect(
-    'mongodb://localhost/myagent_io',
+    process.env.MONGODB_URI || 'mongodb://localhost/myagent_io',
     { useNewUrlParser: true }
 )
 
