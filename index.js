2fe896a9188319ce7a0ce490a02123e205b50e3f
alex-avila <alex.avilx@icloud.com>
2018-08-28 21:12:57 -0600

    Set up express to server react app

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const port = 8000
+const path = require('path')
+const port = process.env.PORT || 8000
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 mongoose.connect(
     'mongodb://localhost/myagent_io',
@@ -18,5 +20,9 @@ db.on('error', console.error.bind(console, 'connection error'))
 db.on('open', () => console.log('Connected to the database'))
 
 app.use('/records', require('./routes/records'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
