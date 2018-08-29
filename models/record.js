eef59d58191aabacae6a6d001f3319871a608eb9
alex-avila <alex.avilx@icloud.com>
2018-08-28 23:36:54 -0600

    Finish latest version of record model

--- models/record.js
+++ models/record.js
@@ -2,96 +2,107 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const recordSchema = new Schema({
+    clientLocation: {
+        address: {
+            type: String,
+            require: true
+        },
+        latLng: {
+            lat: {
+                type: Number,
+                required: true
+            },
+            lng: {
+                type: Number,
+                required: true
+            }
+        }
+    },
     clientType: {
         type: String,
+        required: true,
         enum: ['buyer', 'seller', 'buyer-and-seller']
+    },
+    propertyType: {
+        type: String,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    propertyTypeBuying: {
+        type: String,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    propertyTypeSelling: {
+        type: String,
+        enum: [
+            'single family',
+            'condominium',
+            'townhouse',
+            'vacant lot',
+            'commercial',
+            'other'
+        ]
+    },
+    budget: {
+        type: Number
+    },
+    price: {
+        type: Number
+    },
+    sellingAddress: {
+        address: {
+            type: String,
+            required: true
+        },
+        latLng: {
+            lat: {
+                type: Number,
+                required: true
+            },
+            lng: {
+                type: Number,
+                required: true
+            }
+        }
+    },
+    loan: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ['approved', 'need a lender', 'no thanks']
+    },
+    contact: {
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            lowercase: true,
+            trim: true,
+            required: true
+        },
+        phone: {
+            type: String
+        },
+        additionalInfo: {
+            type: String
+        }
     }
-    // contact: {
-    //     name: {
-    //         type: String,
-    //         trim: true,
-    //         required: true
-    //     },
-    //     email: {
-    //         type: String,
-    //         lowercase: true,
-    //         trim: true,
-    //         required: true
-    //     },
-    //     phone: {
-    //         type: String
-    //     },
-    //     additionalInfo: {
-    //         type: String
-    //     }
-    // },
-    // location: {
-    //     lat: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     lng: {
-    //         type: String,
-    //         required: true
-    //     }
-    // },
-    // budget: {
-    //     type: String
-    // },
-    // price: {
-    //     type: String
-    // },
-    // address: {
-    //     lat: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     lng: {
-    //         type: String,
-    //         required: true
-    //     }
-    // },
-    // propertyType: {
-    //     type: String,
-    //     lowercase: true,
-    //     enum: [
-    //         'single family',
-    //         'condominium',
-    //         'townhouse',
-    //         'vacant lot',
-    //         'commercial',
-    //         'other'
-    //     ]
-    // },
-    // propertyTypeBuyer: {
-    //     type: String,
-    //     lowercase: true,
-    //     enum: [
-    //         'single family',
-    //         'condominium',
-    //         'townhouse',
-    //         'vacant lot',
-    //         'commercial',
-    //         'other'
-    //     ]
-    // },
-    // propertyTypeSeller: {
-    //     type: String,
-    //     lowercase: true,
-    //     enum: [
-    //         'single family',
-    //         'condominium',
-    //         'townhouse',
-    //         'vacant lot',
-    //         'commercial',
-    //         'other'
-    //     ]
-    // },
-    // loan: {
-    //     type: String,
-    //     require: true,
-    //     enum: ['no thanks', 'approved', 'need lender']
-    // }
 })
 
 module.exports = mongoose.model('Record', recordSchema)
