3a96c230fcb3f10a000e2308eebbe191cb3f781c
alex-avila <alex.avilx@icloud.com>
2018-08-28 21:24:16 -0600

    Require dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
