8dfc71e5ca6cb80e5deb1369ca1a792759743b30
alex-avila <alex.avilx@icloud.com>
2018-09-01 23:42:17 -0600

    Separate paragraphs

--- client/src/scenes/Landing/components/PersuasiveInfo/index.js
+++ client/src/scenes/Landing/components/PersuasiveInfo/index.js
@@ -21,14 +21,24 @@ const PersuasiveInfo = () => {
                     <p>
                         With over 2 million Realtors® and real estate agents in
                         the U.S. & Canada, it’s not easy deciding who to trust
-                        when buying or selling a home. A top real estate agent
-                        will set and negotiate the best price for your property
-                        and help you thoroughly understand the local real estate
-                        market. They’ll quickly respond to your needs, help you
-                        find listings before they’re public, mind every little
-                        detail, and earn your respect for life. Whether you’re
-                        selling your current property or buying your dream home,
-                        we’ll help you find the top agent you need.
+                        when buying or selling a home.
+                    </p>
+                    <p>
+                        A top real estate agent will set and negotiate the best
+                        price for your property and help you thoroughly
+                        understand the local real estate market.
+                    </p>
+                    <p>
+                        They’ll quickly respond to your needs, help you find
+                        listings before they’re public, mind every little
+                        detail, and earn your respect for life.
+                    </p>
+                    <p>
+                        <em>
+                            Whether you’re selling your current property or
+                            buying your dream home, we’ll help you find the top
+                            agent you need.
+                        </em>
                     </p>
                 </div>
             </div>
@@ -38,14 +48,21 @@ const PersuasiveInfo = () => {
                     <h3>We Partner With Agents From Over 3,000 Brokerages</h3>
                     <p>
                         Agent Pronto is an independent service that works with
-                        agents across many different brokerages. Over 80% of
-                        real estate firms are independent. We search local
-                        neighborhood and boutique brokerages, as well as larger
-                        firms like Century 21, RE/MAX, and Keller Williams, to
-                        find the best agent for you. Regardless of the brokerage
-                        or its size, we’re committed to finding you the perfect
-                        local agent to assist in buying your next home or
-                        selling your current one.
+                        agents across many different brokerages.
+                    </p>
+                    <p>
+                        Over 80% of real estate firms are independent. We search
+                        local neighborhood and boutique brokerages, as well as
+                        larger firms like Century 21, RE/MAX, and Keller
+                        Williams, to find the best agent for you.
+                    </p>
+                    <p>
+                        <em>
+                            Regardless of the brokerage or its size, we’re
+                            committed to finding you the perfect local agent to
+                            assist in buying your next home or selling your
+                            current one.
+                        </em>
                     </p>
                 </div>
             </div>
