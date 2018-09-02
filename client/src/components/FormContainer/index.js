dcdde6668706300f3179534127d8b8746a5e06f6
alex-avila <alex.avilx@icloud.com>
2018-09-02 14:37:16 -0600

    Change styles so that it works better in landing page using flexbox

--- client/src/components/FormContainer/index.js
+++ client/src/components/FormContainer/index.js
@@ -6,13 +6,7 @@ import background from '../../assets/background.jpg'
 
 import './index.css'
 
-const FormContainer = ({
-    children,
-    slim,
-    staticHeight,
-    staticOverflow,
-    noNav
-}) => {
+const FormContainer = ({ children, slim, staticHeight, staticOverflow }) => {
     const style = {
         background: `
                 linear-gradient(
@@ -20,31 +14,29 @@ const FormContainer = ({
                     rgba(255, 230, 230, 0.25)
                 ),
                 url(${background})`,
-        backgroundPosition: 'right 50% center',
+        backgroundPosition: '70% 100%',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
-        // filter: 'blur(3px)'
     }
     const containerStyle = {}
-    if (staticHeight) {
-        containerStyle.minHeight = 'auto'
-    }
-    if (staticOverflow) {
-        containerStyle.overflow = 'initial'
-    }
+    if (staticHeight) containerStyle.minHeight = 'auto'
     return (
-        <React.Fragment>
-            {noNav ? null : <Nav />}
-            <div className="form-container" style={containerStyle}>
+        <div className="form-container" style={containerStyle}>
+            <Nav />
+            <div className="form-container__wrapper" style={containerStyle}>
                 <div className="form-container__background" style={style} />
                 <div
                     className="form-container__card"
                     style={slim ? { maxWidth: '500px' } : null}
                 >
+                    {/* <div
+                        className="for-container__card--before"
+                        style={style}
+                    /> */}
                     <div className="form-container__wrapper">{children}</div>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
     )
 }
 
